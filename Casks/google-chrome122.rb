f358eef4ed6dbdd188f537b4171585e4b7c3422f
Johnnnnnnnnnn <47521428+Johnnnnnnnnnn@users.noreply.github.com>
2024-03-01 18:43:11 +0800

    Update google-chrome122.rb

--- Casks/google-chrome122.rb
+++ Casks/google-chrome122.rb
@@ -3,7 +3,7 @@ cask "google-chrome122" do
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
-  name "Google Chrome"
+  name "Google Chrome122"
   desc "Web browser"
   homepage "https://www.google.com/chrome/"
 
@@ -17,7 +17,7 @@ cask "google-chrome122" do
   auto_updates false
   depends_on macos: ">= :catalina"
 
-  app "Google Chrome122.app"
+  app "Google Chrome.app"
 
   zap launchctl: [
         "com.google.keystone.agent",
